252752de07ec9be821b47aca23b0132ab889f9ab
Kent Telebanco <ktelebanco@gmail.com>
2022-05-16 11:48:34 +0800

    working addAttendance method

--- GVC API/src/model/attendance.model.ts
+++ GVC API/src/model/attendance.model.ts
@@ -1,1 +1,28 @@
-//TODO ATTENDANCE
+import 'firebase/auth';
+import 'firebase/firestore';
+
+export class Attendance {
+  constructor(
+    private name: string,
+    public id: number,
+    private date: string,
+    public time: string,
+    private classcode: string,
+    private department: string,
+  ) {}
+
+  log(){
+    console.log(`${this.name} ${this.id} ${this.date} ${this.time} ${this.classcode} ${this.department}`);
+}
+
+toJson(){
+    return {
+        name:this.name,
+        employeeId:this.id,
+        date:this.date,
+        time:this.time,
+        classCode:this.classcode,
+        department:this.department
+    };
+}
+}
