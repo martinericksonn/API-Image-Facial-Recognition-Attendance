3ff13e6e27ae7427659930581dfdef8189028572
Kent Telebanco <ktelebanco@gmail.com>
2022-05-17 14:41:34 +0800

    added Attendance method

--- GVC API/src/user.resource/firebase.database.ts
+++ GVC API/src/user.resource/firebase.database.ts
@@ -1,6 +1,7 @@
 import { user } from 'firebase-functions/v1/auth';
 import 'firebase/auth';
 import 'firebase/firestore';
+import { Attendance } from 'src/model/attendance.model';
 import { Account } from 'src/model/account.model';
 import { SystemMessage } from 'src/model/system_message.model';
 import { CRUDReturn } from './crud_return.interface';
@@ -8,6 +9,7 @@ import { CRUDReturn } from './crud_return.interface';
 const admin = require('firebase-admin');
 const systemMessage = new SystemMessage();
 const accounts = 'accounts';
+const attendance = 'attendance';
 
 export class DatabaseQuery {
   static async commit(account: Account): Promise<CRUDReturn> {
@@ -127,6 +129,100 @@ export class DatabaseQuery {
           data.collegeName,
           data.onLeave,
           data.resigned,
+        );
+        populatedData.push(user.toJson());
+      });
+      return systemMessage.success(populatedData);
+    } catch (error) {
+      console.log(error);
+      throw systemMessage.error(error);
+    }
+  }
+
+  //
+  static async commitAttendance(attendances: Attendance): Promise<CRUDReturn> {
+    try {
+      var db = admin.firestore();
+      await db
+        .collection(attendance)
+        .doc(attendances.id.toString())
+        .set(attendances.toJson());
+      console.log('not error');
+      return systemMessage.success(attendances.toJson());
+    } catch (error) {
+      console.log(error);
+      return systemMessage.error(error);
+    }
+  }
+
+  static async getAttendance(id: string) {
+    try {
+      var db = admin.firestore();
+      var userRef = await db.collection(attendance).doc(id).get();
+      if (!userRef.exists) {
+        throw systemMessage.error(506);
+      }
+
+      return userRef.data();
+    } catch (error) {
+      return error;
+    }
+  }
+
+  static async hasID2(id: string): Promise<boolean | any> {
+    try {
+      var db = admin.firestore();
+      const userRef = db.collection(attendance).doc(id);
+      var doc = await userRef.get();
+
+      return !doc.exists;
+    } catch (error) {
+      throw systemMessage.error(error);
+    }
+  }
+
+  static async deleteAttendance(id: string): Promise<CRUDReturn> {
+    try {
+      var db = admin.firestore();
+      await db.collection(attendance).doc(id).delete();
+
+      return systemMessage.success(103);
+    } catch (error) {
+      console.log(error);
+      return systemMessage.error(error);
+    }
+  }
+
+  static async updateAttendance(id: string, attendances: Attendance) {
+    try {
+      var db = admin.firestore();
+      await db.collection(attendance).doc(id).update(attendances);
+
+      var newUser = await db.collection(attendance).doc(id).get();
+      return systemMessage.success(newUser.data());
+    } catch (error) {
+      console.log(error);
+      throw systemMessage.error(error);
+    }
+  }
+
+  static async getAllAttendances() {
+    try {
+      var db = admin.firestore();
+      var userRef = await db.collection(attendance).get();
+
+      var populatedData = [];
+
+      userRef.forEach((doc) => {
+        var data = doc.data();
+
+        var user = new Attendance(
+          data.name,
+          data.id,
+          data.date,
+          data.time,
+          data.classcode,
+          data.department,
         );
         populatedData.push(user.toJson());
       });
--- GVC API/src/user.resource/helper.ts
+++ GVC API/src/user.resource/helper.ts
@@ -4,6 +4,7 @@ import { v4 as uid } from 'uuid';
 // import { Message } from 'src/chat.resource/chat.interface';
 import { SystemMessage } from 'src/model/system_message.model';
 import { Account } from 'src/model/account.model';
+import { Attendance } from 'src/model/attendance.model';
 // import { SystemMessage, User } from '../model/account.model';
 
 export class Helper {
@@ -44,6 +45,42 @@ export class Helper {
 
   static describeClassUser(): Array<any> {
     let a = new Account('', 123, '', '', false, false);
+    let array = Object.getOwnPropertyNames(a);
+
+    return array;
+  }
+
+  //
+  static validAttendanceBody(body: any) {
+    var systemMessage = new SystemMessage();
+
+    var keys: Array<string> = Helper.describeClassUser2();
+    var types: Map<string, string> = new Map<string, string>();
+
+    types.set('name', typeof '');
+    types.set('id', typeof 0);
+    types.set('date', typeof '');
+    types.set('time', typeof '');
+    types.set('classcode', typeof '');
+    types.set('department', typeof '');
+
+    for (const key of Object.keys(body)) {
+      if (!keys.includes(`${key}`) && typeof body[key] != types.get(key)) {
+        throw systemMessage.error(502);
+      }
+      if (typeof body[key] != types.get(key)) {
+        console.log(body[key]);
+        console.log(types.get(key));
+        throw this.systemMessage.custom({
+          success: false,
+          data: `${key} is not a valid attribute`,
+        });
+      }
+    }
+  }
+
+  static describeClassUser2(): Array<any> {
+    let a = new Attendance('', 123, '', '', '','');
     let array = Object.getOwnPropertyNames(a);
 
     return array;
